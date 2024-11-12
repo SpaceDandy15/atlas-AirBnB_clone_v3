23043edd475a478a2c022b7ebd2df6b2d3c5ce2f
Tsimmuaj <tyang4204@gmail.com>
2024-11-12 11:25:18 -0600

    Got states to output a 404

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,11 +8,20 @@ import requests
 app = Flask("__name__")
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///states.db'
+
+
 db = SQLAlchemy(app)
 
 
 class State(db.Model):
     __tablename__ = "states"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
 
     @staticmethod
     def to_dict(state):
