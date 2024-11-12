a102ce294c8824f3280e72360ebc40f8cc274360
Malik Vance <malikovance@gmail.com>
2024-11-12 16:50:39 -0600

    removed the stats import from the init file since its not needed there

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,6 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import everything from the index file to register routes
 from api.v1.views.index import *
-from api.v1.views.stats import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
