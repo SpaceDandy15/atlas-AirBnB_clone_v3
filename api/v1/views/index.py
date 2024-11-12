43a4063edacc7fc57bb12e06eb5bc30869c76524
Malik Vance <malikovance@gmail.com>
2024-11-12 16:25:48 -0600

    for some reason i thought i had to put stats in its own file XD

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,29 @@
 """This file defines routes for API version 1"""
 
 from flask import jsonify
-from api.v1.views import app_views
+from api.v1.views import app_views, storage, valid_models
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of the API as a JSON object"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count_each_model():
+    '''Retrieve and return the number of each object by type'''
+    counts = {}
+    model_names = {
+        'User': 'users',
+        'State': 'states',
+        'City': 'cities',
+        'Place': 'places',
+        'Amenity': 'amenities',
+        'Review': 'reviews'
+    }
+    for name, model in valid_models().items():
+        count = storage.count(model)
+        name = model_names.get(name)
+        counts.update({name: count})
+    return jsonify(counts)
