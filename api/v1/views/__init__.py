9a68ee0d28b59dd6c4df34e1c2a5befe3c099a49
Malik Vance <malikovance@gmail.com>
2024-11-12 23:24:27 -0600

    updated init file and also new view for Review object that handles all default RESTFul API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,14 +4,14 @@ from models import storage
 # Create the blueprint for the app views
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Register the users routes with the blueprint
-from api.v1.views.users import app as users_app  # Import users' app here
-app_views.register_blueprint(users_app)
-
-# Register other blueprints as needed
+# Register blueprints for different app modules
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.places import *
-from api.v1.views.users import *
+from api.v1.views.places_reviews import *
+from api.v1.views.users import app as users_app  # Import users' app and register it here
+
+# Register the users' blueprint
+app_views.register_blueprint(users_app)
