5f3f49315034382e7b1c856368ec8f5a6075e9d8
Malik Vance <malikovance@gmail.com>
2024-11-13 10:05:29 -0600

    added valid models to init.py file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ from models import storage
 # Create the blueprint for the app views
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 storage = storage
+validmodels = validmodels
 # Register blueprints for different app modules
 from api.v1.views.index import *
 from api.v1.views.states import *
