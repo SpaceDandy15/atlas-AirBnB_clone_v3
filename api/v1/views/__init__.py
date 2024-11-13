1b503b143fb9577c7c510d6fb66ac118943cc9b0
Malik Vance <malikovance@gmail.com>
2024-11-12 23:47:53 -0600

    fixed up init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,7 @@ from models import storage
 
 # Create the blueprint for the app views
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+storage = storage
 # Register blueprints for different app modules
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -11,7 +11,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.users import app as users_app  # Import users' app and register it here
-
-# Register the users' blueprint
-app_views.register_blueprint(users_app)
+from api.v1.views.users import *
