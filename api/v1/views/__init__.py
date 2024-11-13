5c07148520752b9401205ab81ae97417f14d2c35
Malik Vance <malikovance@gmail.com>
2024-11-12 22:18:15 -0600

    updated init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,14 @@
-# api/v1/views/__init__.py
 from flask import Blueprint
 from models import storage
-# Create the blueprint
+
+# Create the blueprint for the app views
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Register the users routes with the blueprint
+from api.v1.views.users import app as users_app  # Import users' app here
+app_views.register_blueprint(users_app)
 
+# Register other blueprints as needed
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
