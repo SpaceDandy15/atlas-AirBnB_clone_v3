89d68e18575d2c9855b95b4cc1f8e64980c4a367
Malik Vance <malikovance@gmail.com>
2024-11-12 22:42:48 -0600

    pep 8 api fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,15 +4,17 @@
 from flask import jsonify
 from api.v1.views import app_views  # Keep this import here
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of the API as a JSON object"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def count_each_model():
     '''Retrieve and return the number of each object by type'''
-    
+  
     # Defer the imports to avoid circular import
     from api.v1.views import storage, valid_models
 
